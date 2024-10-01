fb4569f70dd4252a00a2d60cfa64ec4ed1f335f1
Youssef-Jdidi <y.jdidi@betclicgroup.com>
2024-10-01 22:17:47 +0200

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Youssef JDIDI on 01/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
